c8b79c78b31a3cc4c59e89fdffbccc194a8eefe8
Tankariadarsh <tankariadarsh32@gmail.com>
2023-11-10 15:05:17 +0530

    se lab

---
+++ src/main/java/com/kmit/adarsh/App.java
@@ -0,0 +1,13 @@
+package com.kmit.adarsh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "this is se lab internal lab exam" );
+    }
+}
